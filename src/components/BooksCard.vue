80e988aa651ab1a2dbd1e3669f67174349289f5b
Thinkedou <r.enguehard@gmail.com>
2022-11-03 10:35:54 +0100

    nettoyage + card books

---
+++ src/components/BooksCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <figure class="col-lg-3 col-md-4 col-sm-6 col-12 tm-gallery-item">
+    <a href="preview.html">
+        <div class="tm-gallery-item-overlay">
+            <img src="./assets/img/image-01.jpg" alt="Image" class="img-fluid tm-img-center">
+        </div>
+        <p class="tm-figcaption">Book title</p>
+    </a>
+</figure>
+</template>
+
+<style scoped>
+
+</style>
