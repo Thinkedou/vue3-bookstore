683cecbbd35cb030aaab38a02eebee105538020e
Thinkedou <r.enguehard@gmail.com>
2022-11-03 12:26:22 +0100

    watch sur route

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref,computed} from 'vue'
+import {ref,computed,watch} from 'vue'
 import {useRoute,useRouter} from 'vue-router'
 
 import TheHeader from './components/TheHeader.vue'
@@ -13,9 +13,26 @@ const route  = useRoute()
 const router = useRouter()
 
 
+watch(
+  () => route,
+  (newValue, oldValue) => {
+
+
+    refreshBooksList()
+
+
+  },
+  { deep: true }
+)
+
+const refreshBooksList = ()=>{
+    console.log('on va refresh avec la page: ', route.query.page)
+}
+
 const localBookstore = ref(bookstore)
 
 const displayBooks = computed(()=>{
+
     return localBookstore.value.slice(0,8)
 })
 
@@ -33,7 +50,7 @@ const handlePagination = page =>{
             <div class="tm-main-content">
                 <section class="tm-margin-b-l">
                     <header>
-                        <h2 class="tm-main-title">Welcome to our bookstore</h2>    
+                        <h2 class="tm-main-title">Welcome to our bookstore Page: {{route.query.page}}</h2>    
                     </header>
                     
                     <p>Shelf HTML template is provided by Tooplate. Please tell your friends about it. Thank you. Images are from Unsplash website. In tincidunt metus sed justo tincidunt sollicitudin. Curabitur magna tellus, condimentum vitae consectetur id, elementum sit amet erat.</p>
@@ -53,7 +70,7 @@ const handlePagination = page =>{
                         <ul class="nav justify-content-center">
                             <li class="nav-item" @click="handlePagination(1)"><span class="nav-link active" >1</span></li>
                             <li class="nav-item" @click="handlePagination(2)" ><span class="nav-link">2</span></li>
-                            <li class="nav-item"><a class="nav-link" href="#">3</a></li>
+                            <li class="nav-item" @click="handlePagination(3)"><a class="nav-link" href="#">3</a></li>
                             <li class="nav-item"><a class="nav-link" href="#">4</a></li>                    
                         </ul>
                     </nav>
